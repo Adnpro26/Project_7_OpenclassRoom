9ea61bb216c1c0b63ff1686760d121cf6863bc5b
Adnan O <a.outanine@gmail.com>
2024-05-16 13:47:54 +0200

    import json split dict pour prediction

--- app.py
+++ app.py
@@ -1,40 +1,63 @@
 from flask import Flask, request, jsonify
+import pickle
+import numpy as np
 
 import pandas as pd
 #import imblearn
 import lightgbm as lgb
-
-#import pickle
 import joblib
-import pandas as pd
+
+import json
+
 
 app = Flask(__name__)
+
+
+#with open('/home/aoutanine/Project_7_OpenclassRoom/best_model_parameters.pkl', 'rb') as model_file:
+    #model = pickle.load(model_file)
+
 model = joblib.load('/home/aoutanine/Project_7_OpenclassRoom/best_model_parameters.pkl')
 
-def chargement_donnees():
-    X_test=pd.read_csv("test_data.csv")
-    donnees = X_test.to_dict(orient='records')
-    return donnees
 
-@app.route("/")
-def home():
-    return "Prédiction risque de crédit"
-
-@app.route('/donnees-test', methods=['GET'])
-def input_test_data():
-    donnees = chargement_donnees()
-    return jsonify(donnees)
-
+# Define a route for prediction
 @app.route('/predict', methods=['POST'])
 def predict():
-    
-    input_data = request.json["donnnes"]
+    # Get data from request
+    data = request.json
+  
+    # Preprocess data if needed
+    # Example: Convert data to numpy array
+    features = data["features"]
 
-    
-    prediction = model.predict(input_data)
+    df_json_text = pd.DataFrame([features])
+    df_json_text = df_json_text.to_dict(orient='list')
+    n = 0
+    resultat = {}
+    label_values = []
+    values_customer = []
+    for cle, texte in df_json_text.items():
+        for elements in texte:
+            element_to_dict = json.loads(elements)
+            for c, t in element_to_dict.items():
+                label_values.append(c)
+                for c1,t1 in t.items():
+                    values_customer.append(t1)
 
+    df_customers = pd.DataFrame([values_customer], columns=label_values)
+
+    #type_data = type(features)
+
+    #print(features)
     
-    return jsonify({'prediction': prediction.tolist()}), 200
+    # Make prediction
+    #prediction = model.predict(features.reshape(1, -1))
+    prediction = model.predict(df_customers)
+    
+    #Return prediction
+    return jsonify({'prediction': prediction.tolist()})
+    #return jsonify({'prediction': features})
+
+    #return features
 
 if __name__ == '__main__':
     app.run(debug=True)
